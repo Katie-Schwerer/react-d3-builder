50146b2267572772463c7bd3424db9ca223dbbab
Katie Schwerer <89220941+Katie-Schwerer@users.noreply.github.com>
2026-02-09 18:26:22 +0000

    Original idea, may use this another time

---
+++ chart-builder/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <input type="file" id="fileInput" name="fileInput" accept=".csv"/>
+    </div>
+  );
+}
+
+export default App;
